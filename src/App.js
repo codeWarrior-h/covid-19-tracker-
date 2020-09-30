3fd0692547711ffa5c5338ce6c8c4c486f19c6c7
codeWarrior-h <harshmandar55@gmail.com>
2020-10-01 00:32:22 +0530

    Selecting feature in dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React , {useState , useEffect} from 'react';
 import './App.css';
-import { FormControl , Select , MenuItem } from '@material-ui/core';
-
+import { FormControl , Select , MenuItem } from "@material-ui/core";
 
 
 function App() {
@@ -9,6 +8,8 @@ function App() {
   const [countries , setCountries] =  useState([]);
   // STATE = how to write a variable in react
   //it temporarily stores the data
+
+ const [country , setCountry] = useState('worldwide');
 
 //useEffect runs a piece of code for a given condition
 //it fires when the app is load first
@@ -35,6 +36,14 @@ function App() {
   //the code inside here will run once when component loads and not again after
   // if [] is empty but if [] contains countries so it will run when countries change
 
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+
+    console.log('yuuuup' , countryCode);
+
+    setCountry(countryCode);
+  }
+
   return (
     <div className="app">
      <div className = "app_header">
@@ -43,25 +52,18 @@ function App() {
         {/* Title + dropdown field */}
       <h1>COVID-19 TRACKER</h1>
 
-      <FormControl className = "app_dropdown"> {/*to make the dropdown menu  */}
-
-        <Select variant = "outlined" value = "" > {/* for selecting  */}
-
-        {/*  <MenuItem value = "worldwide">Worldwide</MenuItem>  items available for selecting
-          <MenuItem value = "worldwide">option 2</MenuItem>
-          <MenuItem value = "worldwide">option 3</MenuItem>
-          <MenuItem value = "worldwide">hey</MenuItem>*/}
-
-         {/*Loop through all the countries and show dropdown menu list of options*/}
-           {
-             countries.map(country => (
-               <MenuItem value = {country.value}>{country.name}</MenuItem>
-             ))
-           }
-
-        </Select>
-
-      </FormControl>
+      <FormControl className="app_dropdown">
+           <Select
+             variant="outlined"
+             value={country}
+             onChange={onCountryChange}
+           >
+             <MenuItem value="worldwide">Worldwide</MenuItem>
+             {countries.map((country) => (
+               <MenuItem value={country.value}>{country.name}</MenuItem>
+             ))}
+           </Select>
+         </FormControl>
 
 
       {/* InfoBox */}
